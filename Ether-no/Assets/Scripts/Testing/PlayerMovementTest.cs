0f02a5a3f2f67a7e28b1200b8b0c7b8742ab5118
Maira Rodas <maira.rodas@udea.edu.co>
2024-12-26 16:04:17 -0500

    Add folder structure & simple attack

---
+++ Ether-no/Assets/Scripts/Testing/PlayerMovementTest.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementTest : MonoBehaviour
+{
+    public float speed = 5;
+
+    //For visualizing the hit position
+    public Transform target;
+
+    void Update()
+    {
+        //Movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        direction.Normalize();
+        transform.position += direction * speed * Time.deltaTime;
+
+        //Rotation
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+
+            //For visualizing the hit position
+            target.position = new Vector3(hit.point.x, 0, hit.point.z);
+        }
+     
+    }
+}
