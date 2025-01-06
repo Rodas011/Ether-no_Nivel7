ee0770126e93077a1b445e4a07da67cca3306314
Maira Rodas <maira.rodas@udea.edu.co>
2025-01-06 14:31:21 -0500

    Add enemy movement

--- Ether-no/Assets/Scripts/Enemies/EnemyMovement.cs
+++ Ether-no/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Stats
+    public float chaseRange = 1000f;
+
+    private bool playerInChaseRange;
+    private Transform player;
+    private LayerMask whatIsPlayer;
+    private NavMeshAgent agent;
+
+    private void Awake()
     {
-        
+        player = GameObject.FindWithTag("Player").transform;
+        whatIsPlayer = LayerMask.GetMask("whatIsPlayer");
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Chase when the player is in range
+        playerInChaseRange = Physics.CheckSphere(transform.position, chaseRange, whatIsPlayer);
+
+        if (playerInChaseRange)
+        {
+            Chase();
+        }
     }
+
+    private void Chase()
+    {
+        transform.LookAt(player.position);
+        agent.SetDestination(player.position);
+    }
+
+
 }
