ab6f74eab6af7dde2857a4d486e8f494d310fbb9
Maira Rodas <maira.rodas@udea.edu.co>
2025-01-13 11:42:19 -0500

    Add dragon and multi-attack

--- Ether-no/Assets/Scripts/Enemies/EnemyAttack.cs
+++ Ether-no/Assets/Scripts/Enemies/EnemyAttack.cs
@@ -9,6 +9,8 @@ public class EnemyAttack : MonoBehaviour
     public Transform attackPoint;
     public float shootForce = 10f;
     public float attackRange = 10f;
+    public int numberOfBullets = 5;
+    public float bulletSpread = 10f;
 
     private float frecuency; //Time between shots
     private bool readyToShoot = true;
@@ -23,7 +25,7 @@ public class EnemyAttack : MonoBehaviour
         player = GameObject.FindWithTag("Player").transform;
         whatIsPlayer = LayerMask.GetMask("whatIsPlayer");
 
-        //Get the frecuency from PlayerController
+        //Get the frecuency from EnemyController
         controller = GetComponent<EnemyController>();
         frecuency = controller.frecuency;
     }
@@ -32,7 +34,7 @@ public class EnemyAttack : MonoBehaviour
     {
         //Shoot when the player is in range
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
-        
+
         if (playerInAttackRange && readyToShoot)
         {
             Shoot();
@@ -45,16 +47,26 @@ public class EnemyAttack : MonoBehaviour
 
         transform.LookAt(player);
 
-        //Instantiate the bullet
-        Vector3 direction = transform.forward;
-        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
-        currentBullet.transform.forward = direction.normalized;
+        //Calculate the start rotation of the bullets
+        float facingRotation = transform.eulerAngles.y;
+        float startRotation = facingRotation + bulletSpread / 2f;
 
-        //Set the damage of the bullet
-        currentBullet.GetComponent<BulletController>().damage = controller.damage;
+        //Calculate the angle increase between bullets
+        float angleIncrease = bulletSpread / ((float) numberOfBullets);
 
-        //Add force to the bullet
-        currentBullet.GetComponent<Rigidbody>().AddForce(direction.normalized * shootForce, ForceMode.Impulse);
+        //Instantiate the bullets
+        for (int i = 0; i < numberOfBullets; i++)
+        {
+            //Calculate the rotation of the bullet
+            float rotation = startRotation - angleIncrease * i;
+            GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.Euler(0f, rotation, 0f));
+
+            //Set the damage of the bullet
+            currentBullet.GetComponent<BulletController>().damage = controller.damage;
+
+            //Add force to the bullet
+            currentBullet.GetComponent<Rigidbody>().AddForce(currentBullet.transform.forward * shootForce, ForceMode.Impulse);
+        }
 
         Invoke("ResetShoot", frecuency);
     }
