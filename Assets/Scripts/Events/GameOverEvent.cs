0cdc131b20b074786acac2903e757acd2a962bbd
Maira Rodas <maira.rodas@udea.edu.co>
2025-01-14 13:36:44 -0500

    Change project structure

--- Assets/Scripts/Events/GameOverEvent.cs
+++ Assets/Scripts/Events/GameOverEvent.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOver : MonoBehaviour
+public class GameOverEvent : MonoBehaviour
 {
     [SerializeField] private GameState gameState;
     private GameObject gameOverCanvas;
--- Assets/Scripts/Events/PauseEvent.cs
+++ Assets/Scripts/Events/PauseEvent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pause : MonoBehaviour
+public class PauseEvent : MonoBehaviour
 {
     [SerializeField] private GameState gameState;
     private GameObject pauseCanvas;
